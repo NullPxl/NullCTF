5cfe010513eda218f766ff792d670cd0ccc21e26
nullpxl <nullpxl.mail@gmail.com>
2018-08-16 10:56:35 -0700

    db.txt is now db.json, updated ctfs current cog to fetch data from this file.

--- cogs/ctfs.py
+++ cogs/ctfs.py
@@ -144,97 +144,83 @@ class Ctfs():
 {leaderboards}```''')
         
         if status == 'current':
-            def updateDb():
-                upcoming = 'https://ctftime.org/api/v1/events/'
-                limit = '5'
-                response = requests.get(upcoming, headers=headers, params=limit)
-                json_data = response.json()
+            # Update db.json
+            headers = {
+                    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0',
+                    }
+            upcoming = 'https://ctftime.org/api/v1/events/'
+            limit = '5'
+            response = requests.get(upcoming, headers=headers, params=limit)
+            json_data = response.json()
+            data = []
+            def update(name, entry):
+                with open("db.json", mode='a', encoding='utf-8') as db:
+                    json.dump(data, db, indent=3)
 
-                with open('db.txt', 'r+') as db:
-                    data = db.read()
-                    
-                    for num in range(0, int(limit)):
-                        ctf_title = json_data[num]['title']
-                        ctf_check = f'''{ctf_title}'''
-                        
-                        if ctf_check in data:
-                            continue
-                        else:
-                            now = datetime.utcnow()
-                            unix_now = int(now.replace(tzinfo=timezone.utc).timestamp())
-                            ctf_format = json_data[num]['format']
-                            ctf_place = json_data[num]['onsite']
-                            
-                            if ctf_place == False:
-                                ctf_place = 'Online'
-                            else:
-                                ctf_place = 'Onsite'
-                            
-                            dur_dict = json_data[num]['duration']
-                            (ctf_hours, ctf_days) = (str(dur_dict['hours']), str(dur_dict['days']))
-                            (ctf_start, ctf_end) = (parse(json_data[num]['start'].replace('T', ' ').split('+', 1)[0]), parse(json_data[num]['finish'].replace('T', ' ').split('+', 1)[0]))
-                            (unix_start, unix_end) = (int(ctf_start.replace(tzinfo=timezone.utc).timestamp()), int(ctf_end.replace(tzinfo=timezone.utc).timestamp()))
-                            dur_dict = json_data[num]['duration']
-                            (ctf_hours, ctf_days) = (str(dur_dict['hours']), str(dur_dict['days']))
-                            ctf_link = json_data[num]['url']
-                            ctf_image = json_data[num]['logo']
-                            db.write(f'''
-<name>{ctf_title}<name> <start>{unix_start}<start> <end>{unix_end}end <link>{ctf_link}<link> <image>{ctf_image}<image> <format>{ctf_place} {ctf_format}<format> <duration>{ctf_days} days, {ctf_hours} hours<duration>''')
-            updateDb()
-            running = False
-            
-            with open('db.txt', 'r+') as db:
-                data = db.readlines()
-                start_pat = r'<start>(.*)<start>'
-                end_pat =  r'<end>(.*)<end>'
-                ctf_pat = r'<name>(.*)<name>'
-                ctf_url_pat = r'<link>(.*)<link>'
-                ctf_img_pat = r'<image>(.*)<image>'
-                ctf_format_pat = r'<format>(.*)<format>'
-                ctf_dur_pat = r'<duration>(.*)<duration>'
-                
-                for line in data:
+            with open("db.json", 'w') as db:
+
+                for num in range(0, int(limit)):
+                    ctf_title = json_data[num]['title']
                     now = datetime.utcnow()
                     unix_now = int(now.replace(tzinfo=timezone.utc).timestamp())
+                    ctf_format = json_data[num]['format']
+                    ctf_place = json_data[num]['onsite']
                     
-                    try:
-                        ctf_name = re.search(ctf_pat, line)
-                        ctf_url = re.search(ctf_url_pat, line)
-                        ctf_img = re.search(ctf_img_pat, line)
-                        ctf_format = re.search(ctf_format_pat, line)
-                        ctf_dur = re.search(ctf_dur_pat, line)
-                        ctf_start = re.search(start_pat, line)
-                        ctf_end = re.search(end_pat, line)
-                        u_start = int(ctf_start.group(0).replace('<start>', ''))
-                        u_end = int(ctf_end.group(0).replace('<end>', ''))
-                        start = datetime.utcfromtimestamp(u_start).strftime('%Y-%m-%d %H:%M:%S')
-                        end = datetime.utcfromtimestamp(u_end).strftime('%Y-%m-%d %H:%M:%S')
-                        url = ctf_url.group(0).replace('<link>', '')
-                        img = ctf_img.group(0).replace('<image>', '')
-                        format_c = ctf_format.group(0).replace('<format>', '')
-                        dur = ctf_dur.group(0).replace('<duration>', '')
-                        ctf = ctf_name.group(0).replace('<name>', '')
-                    except:
-                        continue
+                    if ctf_place == False:
+                        ctf_place = 'Online'
+                    else:
+                        ctf_place = 'Onsite'
                     
-                    if (u_start < unix_now) and (u_end > unix_now):
+                    dur_dict = json_data[num]['duration']
+                    (ctf_hours, ctf_days) = (str(dur_dict['hours']), str(dur_dict['days']))
+                    (ctf_start, ctf_end) = (parse(json_data[num]['start'].replace('T', ' ').split('+', 1)[0]), parse(json_data[num]['finish'].replace('T', ' ').split('+', 1)[0]))
+                    (unix_start, unix_end) = (int(ctf_start.replace(tzinfo=timezone.utc).timestamp()), int(ctf_end.replace(tzinfo=timezone.utc).timestamp()))
+                    dur_dict = json_data[num]['duration']
+                    (ctf_hours, ctf_days) = (str(dur_dict['hours']), str(dur_dict['days']))
+                    ctf_link = json_data[num]['url']
+                    ctf_image = json_data[num]['logo']
+                    ctf_info = {
+                        'name': ctf_title,
+                        'start': unix_start,
+                        'end': unix_end,
+                        'dur': ctf_days+' days, '+ctf_hours+' hours',
+                        'url': ctf_link,
+                        'img': ctf_image,
+                        'format': ctf_place+' '+ctf_format
+                        }
+                    data.append(ctf_info)
+                    
+            update('db.json', data)
+
+            running = False
+            #read from file and check if ctf is running
+            with open("db.json") as db:
+                data = json.load(db) #data, ctf['name'], ctf['start'], ctf['end'], ctf['dur'], ctf['url'], ctf['img'], ctf['format']
+                
+                for ctf in data:
+                    now = datetime.utcnow()
+                    unix_now = int(now.replace(tzinfo=timezone.utc).timestamp())
+                    start = datetime.utcfromtimestamp(ctf['start']).strftime('%Y-%m-%d %H:%M:%S') + ' UTC'
+                    end = datetime.utcfromtimestamp(ctf['end']).strftime('%Y-%m-%d %H:%M:%S') + ' UTC'
+                  
+                    if ctf['start'] < unix_now and ctf['end'] > unix_now: #check if ctf is currently running
+                        print('test')
                         running = True
                         embed = discord.Embed(title=(':red_circle: ' + ctf) + ' IS LIVE', description=url, color=15874645)
-                        
-                        if img != '':
+                     
+                        if ctf['img'] != '':
                             embed.set_thumbnail(url=img)
                         else:
                             embed.set_thumbnail(url=default_image)
-                        
-                        embed.add_field(name='Duration', value=dur, inline=True)
-                        embed.add_field(name='Format', value=format_c, inline=True)
-                        embed.add_field(name='─' * 23, value=((start + ' UTC -> ') + end) + ' UTC', inline=True)
+                         
+                        embed.add_field(name='Duration', value=ctf['dur'], inline=True)
+                        embed.add_field(name='Format', value=ctf['format'], inline=True)
+                        embed.add_field(name='─' * 23, value=start+' -> '+end, inline=True)
                         await ctx.channel.send(embed=embed)
-            
+                        
             if running == False:
                 await ctx.send("There are currently no running ctfs on ctftime.org :neutral_face: .  Check out '>ctftime upcoming' to see upcoming ctfs")
-            else:
-                pass
+ 
         
         if status not in current_ctftime_cmds:
             await ctx.send('Current ctftime commands are: top [year], upcoming [amount up to 5], current')
